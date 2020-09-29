8a0ad5b900481fd07c6a99ed773260decde2a5ad
Ingremsch <gottkehaskamp.hendrik@outlook.com>
2020-09-29 16:36:39 +0200

    .

--- data.py
+++ data.py
@@ -5,7 +5,8 @@ import adafruit_bme680
 
 # Create library object using our Bus I2C port
 i2c = I2C(board.SCL, board.SDA)
-bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+#bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
 bme680.sea_level_pressure = 1013.25
@@ -16,7 +17,8 @@ bme680.sea_level_pressure = 1013.25
 temperature_offset = -5
 
 while True:
-    print("\nTemperature: %0.1f C" % bme680.temperature + temperature_offset)
+    #print("\nTemperature: %0.1f C" % bme680.temperature + temperature_offset)
+    print("\nTemperature: %0.1f C" % bme680.temperature)
     print("Gas: %d ohm" % bme680.gas)
     print("Humidity: %0.1f %%" % bme680.humidity)
     print("Pressure: %0.3f hPa" % bme680.pressure)
